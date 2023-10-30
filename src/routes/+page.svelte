4abba739e4d47d13c53cef35133423fd6fd4da68
David <davidscreation1@gmail.com>
2023-10-30 14:01:03 +0000

    Testing buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    let clickHandle = () => {
+        alert("I am a button!")
+    }
+</script>
+
+<h1>Insert Stuff here ;)</h1>
+<button on:click={clickHandle}>Press me daddy</button>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
